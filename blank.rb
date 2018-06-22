35698529cdc4456fd53282c4f5df706d8dd644f8
coyalo03 <coyalo86@gmail.com>
2018-06-22 20:04:04 +0000

    automatically backed up by learn

---
+++ blank.rb
@@ -0,0 +1,10 @@
+amazon_cart = [57.65, 102.99, 3.99, 25.00, 70.00]
+
+count = 1 
+
+amazon_cart.each do |price|
+  puts "Item #{count} costs #{price}"
+  count += 1 
+end 
+.each do || iterates through an array meaning it will start at the 0th element, and go through 
+each element in order and do something to it.
